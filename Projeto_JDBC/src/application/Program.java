0d5333d625174159e462ce38c8859f98e5bbe344
gbsalermo <enzogabrielskull@gmail.com>
2026-01-29 00:45:00 -0300

    Implementação de update

--- Projeto_JDBC/src/application/Program.java
+++ Projeto_JDBC/src/application/Program.java
@@ -43,12 +43,20 @@ public class Program {
 			System.out.println(obj);
 		}
 		System.out.println("\n=== TESTE 4: SELLER insert ====");
-		
+	/*	
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		*/
+		System.out.println("\n=== TESTE 5: SELLER Updade ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Bruce Wayne");
+		sellerDao.update(seller);
 		
-
+		System.out.println("Update completed");
+		
+		
+		
 	}
 
 }
--- Projeto_JDBC/src/model/dao/impl/SellerDaoJDBC.java
+++ Projeto_JDBC/src/model/dao/impl/SellerDaoJDBC.java
@@ -72,7 +72,32 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void update(Seller obj) {
+	PreparedStatement st = null;
 		
+		try {
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?"); //Para retornar o id do novo vendedor inserido
+			
+			//Pego os valores de cada interrogação
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3,new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId()); //Navego o objeto pra pegar o Id
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}
+		
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
